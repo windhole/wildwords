96787eff9d5280ea0b74bece5b9ffc85bbe97e53
Ko Kazaana <4735171+windhole@users.noreply.github.com>
2026-01-11 02:04:45 +0000

    feat: add tailwind v4 and global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://windhole.github.io/',
-    base: '/wildwords/',
+  server: {
+      port: 4321,
+      host: true,
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
